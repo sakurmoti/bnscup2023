cf5bca02f1884a662a9427bdbbc9589e9f1e6b99
sakurmoti <105205684+sakurmoti@users.noreply.github.com>
2023-11-02 00:36:14 +0900

    physicsを追加

--- Main.cpp
+++ Main.cpp
@@ -2,20 +2,120 @@
 
 double t = 0.0;
 
-struct Prize
+struct Physics
+{
+	double mass;
+	Vec3 pos;
+	Vec3 force;
+
+	Physics(double _mass, Vec3 _pos, Vec3 _force)
+		: mass(_mass), pos(_pos), force(_force)
+	{
+	}
+
+	void update()
+	{
+		// 抵抗が強すぎるので、力を入れないと動かない世界を想定
+		Vec3 nex = pos + ((force / mass) * pow(Scene::DeltaTime(), 2));
+		pos = nex;
+
+		// 力を減衰させて徐々に減速。速度は保存されないものとする
+		force *= 0.90;
+	}
+
+	void setForce(Vec3 _force)
+	{
+		force = _force;
+	}
+
+	void revX()
+	{
+		force.x *= -1;
+	}
+
+	void revY()
+	{
+		force.y *= -1;
+	}
+
+	void revZ()
+	{
+		force.z *= -1;
+	}
+
+	void rev()
+	{
+		force *= -1;
+	}
+
+	Vec3 detect(Physics &obj)
+	{
+		double e = 0.5; //反発係数
+		double m1 = this->mass;
+		double m2 = obj.mass;
+		double M = (m1 * m2) / (m1 + m2);
+
+		Vec3 f1 = this->force;
+		Vec3 f2 = obj.force;
+
+		Vec3 v1 = f1 / m1;
+		Vec3 v2 = f2 / m2;
+
+		// 衝突面の法線ベクトル objからthisへの向き
+		Vec3 n = (this->pos - obj.pos).normalize();
+
+		// 撃力を計算する
+		Vec3 F = ( (1 + e) * M * (v1 - v2) * n ) + (M * (f1/m1 - f2/m2) * n ) * (n) * (1/Scene::DeltaTime());
+
+		// 撃力を物体に加える
+		this->setForce(this->force + F);
+		obj.setForce(obj.force - F);
+		
+		return F;
+	}
+
+
+};
+
+struct Phy_Test : public Physics
+{
+	Mesh mesh;
+
+	Phy_Test(Mesh _mesh, double _mass, Vec3 _pos, Vec3 _force)
+		: Physics(_mass, _pos, _force), mesh(_mesh)
+	{
+	}
+	
+
+	void draw() const
+	{
+		mesh.draw(pos, Palette::Red);
+	}
+
+	
+};
+
+struct Prize : public Physics
 {
 	Mesh type;
 	Vec3 pos;
+	double th0; // 初期位相
+
+	Prize(const Mesh& _type, const Vec3& _pos, const Vec3& _force, const double& _th0)
+		: Physics(1.0, _pos, _force), type(_type), pos(_pos), th0(_th0)
+	{
+	}
 
 	void draw() const
 	{
 		type.draw(pos, Palette::Aquamarine);
 	}
 
-	void update(double radius, double w_rad)
+	void update(double w_rad)
 	{
 		// yを固定軸としてxz平面で等速円運動
-		Vec3 nex = { radius * Math::Cos(w_rad * t), pos.y, radius * Math::Sin(w_rad * t) };
+		double radius = Math::Sqrt(pos.x * pos.x + pos.z * pos.z);
+		Vec3 nex = { radius * Math::Cos(w_rad * t + th0), pos.y, radius * Math::Sin(w_rad * t + th0)};
 		pos = nex;
 	}
 };
@@ -39,7 +139,7 @@ struct Machine
 	{
 		for (auto& g : goods)
 		{
-			g.update(radius, w_rad);
+			g.update(w_rad);
 		}
 	}
 };
@@ -48,16 +148,24 @@ struct Detection
 {
 	Vec3 pos;
 	double radius;
+	Sphere sphere;
 
 	void draw() const
 	{
-		Sphere{ pos, radius }.draw(Palette::Black);
+		sphere.draw(Palette::Black);
 	}
 
-	void detect()
+	void update()
 	{
-		// Machineのgoodsと衝突判定
+		sphere = Sphere(pos, radius);
+	}
 
+	void detect(const Machine &machine)
+	{
+		for (auto& g : machine.goods)
+		{
+			
+		}
 	}
 
 };
@@ -74,7 +182,6 @@ struct Capture
 	Capture(const Vec3& _pos, const Vec3& _center, const double& _w_rad, const double& _radius, const Mesh& _hemisphere)
 		: pos(_pos), center(_center), w_rad(_w_rad), radius(_radius), hemisphere(_hemisphere)
 	{
-		// ここで検出器の位置を初期化?
 		detection.pos = pos;
 		detection.radius = hemisphere.boundingSphere().r - 0.1;
 	}
@@ -90,9 +197,10 @@ struct Capture
 	{
 		// zを固定軸としてxy平面で回転運動のシミュレーション
 		// とりあえず等速円運動
-		Vec3 nex = { radius * Math::Cos(w_rad * t), radius * Math::Sin(w_rad * t), pos.z };
+		Vec3 nex = { radius * Math::Cos(w_rad * t) + center.x, radius * Math::Sin(w_rad * t) + center.y, pos.z };
 		pos = nex;
 		detection.pos = pos;
+		detection.update();
 	}
 };
 
@@ -102,35 +210,43 @@ void Main()
 	const ColorF backgroundColor = ColorF{ 0.4, 0.6, 0.8 }.removeSRGBCurve();
 	const Texture uvChecker{ U"example/texture/uv.png", TextureDesc::MippedSRGB };
 	const MSRenderTexture renderTexture{ Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes };
-	DebugCamera3D camera{ renderTexture.size(), 120_deg, Vec3{ 16, 18, -32 } };
+	DebugCamera3D camera{ renderTexture.size(), 120_deg, Vec3{ 0, 3, 10 } };
 
 	Array<Prize> goods;
-	for (int i = 0; i < 10; i++)
+	const Mesh torus{ MeshData::Torus(6.0, 2.0) };
+	for (int i = 0; i < 500; i++)
 	{
 		Mesh obj;
 		const Array<String> type = { U"Sphere", U"Box", U"Pyramid" };
 		String tp = type.choice();
 
+		// トーラスの一般式 中心までの距離D, 環の半径R, 環の中の角度u, 中心までの角度vとする
+		double D = 5.0;
+		double R = 1.0;
+		double u = Random(Math::Pi);
+		double v = Random(Math::TwoPi);
+		double x = (D + R * Math::Cos(u)) * Math::Cos(v);
+		double z = (D + R * Math::Cos(u)) * Math::Sin(v);
+		double y = R * Math::Sin(u);
+		Vec3 pos = { x, y, z };
+
 		if (tp == U"Sphere")
 		{
-			// 中心[0,x0] [0,y0] [0,z0], 半径rの球を生成
-			int x0 = 10;
-			int y0 = 10;
-			int z0 = 10;
-			int r = 3;
-			obj = (Mesh)MeshData::Sphere(RandomVec3(x0, y0, z0), r);
+			// 中心pos, 半径rの球を生成
+			double r = 0.2;
+			obj = (Mesh)MeshData::Sphere(r);
 		}
 		else if (tp == U"Box")
 		{
 			// 辺の長さlの立方体を生成
-			int l = 3;
+			double l = 0.3;
 			obj = (Mesh)MeshData::Box(l);
 		}
 		else if (tp == U"Pyramid")
 		{
 			// 底辺l, 高さhの四角錐を生成
-			int l = 3;
-			int h = 3;
+			double l = 0.3;
+			double h = 0.3;
 			obj = (Mesh)MeshData::Pyramid(l, h);
 		}
 		else
@@ -138,13 +254,19 @@ void Main()
 			Print << U"Not Found Error in goods select";
 		}
 
-		Vec3 pos = RandomVec3(10);
-		goods.push_back(Prize(obj, pos));
+		goods.push_back(Prize(obj, pos, Vec3{ 0,0,0 }, v));
 	}
-	Machine machine{ Vec3{ 0, 0, 0 }, 0.1, 6.0, goods };
+	// machine{ center, w_rad, radius, goods }
+	Machine machine{ Vec3{ 0, 0, 0 }, (1080_deg * Math::Pi / 180.0), 20.0, goods };
 
-	Mesh hemisphere{ MeshData::Hemisphere(10.0) };
-	Capture capture{ Vec3{ 0, 10, 0 }, Vec3{ 0, 10, 0 }, (1080_deg * Math::Pi / 180.0), 10.0 , hemisphere };
+	Mesh hemisphere{ MeshData::Hemisphere(1.0) };
+	// capture{ pos, center, w_rad, radius, hemisphere }
+	Capture capture{ Vec3{ 0, 0, 0 }, Vec3{ 0, 1.5, 5 }, 10*(360_deg * Math::Pi / 180.0), 1.0 , hemisphere };
+
+	Phy_Test test1{ (Mesh)MeshData::Box(1.0), 1.0, Vec3{ -20, -10, 0 }, Vec3{ 50, 0, 0 } };
+	Phy_Test test2{ (Mesh)MeshData::Box(1.0), 1.0, Vec3{ 20, -10, 0 }, Vec3{ -50, 0, 0 } };
+
+
 
 	while (System::Update())
 	{
@@ -156,11 +278,28 @@ void Main()
 		{
 			const ScopedRenderTarget3D target{ renderTexture.clear(backgroundColor) };
 			Plane{ Vec3{0,-30, 0}, 64 }.draw(uvChecker);
+			//torus.draw({ 0, 0, 0 }, Quaternion::RotateY(t * 30_deg), uvChecker);
 			{
 				const ScopedRenderStates3D blend{ BlendState::OpaqueAlphaToCoverage };
+				machine.update();
 				machine.draw();
+
 				capture.update();
 				capture.draw(uvChecker);
+				capture.detection.detect(machine);
+			}
+
+			{
+				test1.update();
+				test1.draw();
+				test2.update();
+				test2.draw();
+
+				if (Key1.down()) {
+					Print << test1.detect(test2);
+					
+				}
+
 			}
 		}
 
