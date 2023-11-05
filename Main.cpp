940e7ae05cf81d10c5aae7020fd7cfe39145b99b
sakurmoti <105205684+sakurmoti@users.noreply.github.com>
2023-11-05 13:43:40 +0900

    Physicsの追加

--- Main.cpp
+++ Main.cpp
@@ -1,207 +1,265 @@
 ﻿# include <Siv3D.hpp>
 
-double t = 0.0;
-
 struct Physics
 {
 	double mass;
 	Vec3 pos;
+	Vec3 vel;
 	Vec3 force;
+	OrientedBox obj;
 
-	Physics(double _mass, Vec3 _pos, Vec3 _force)
-		: mass(_mass), pos(_pos), force(_force)
+	Physics() : mass(0), vel({ 0,0,0 }), force({ 0,0,0 }), obj(OrientedBox{ {0,0,0},{1,1,1} })
 	{
+		pos = obj.center;
 	}
 
-	void update()
+	/// @param mass 質量
+	/// @param vel 速度
+	/// @param force 力
+	/// @param obj OrientedBox: {center, size}
+	Physics(double mass, Vec3 vel, Vec3 force, OrientedBox obj)
+		: mass(mass), vel(vel), force(force), obj(obj)
 	{
-		// 抵抗が強すぎるので、力を入れないと動かない世界を想定
-		Vec3 nex = pos + ((force / mass) * pow(Scene::DeltaTime(), 2));
-		pos = nex;
-
-		// 力を減衰させて徐々に減速。速度は保存されないものとする
-		force *= 0.90;
+		pos = obj.center;
 	}
 
-	void setForce(Vec3 _force)
+	virtual void draw() const
 	{
-		force = _force;
+		//Print << U"Physics: virtual draw";
+		obj.draw();
 	}
 
-	void revX()
+	virtual void draw(const Texture& tex) const
 	{
-		force.x *= -1;
+		obj.draw(tex);
 	}
 
-	void revY()
+	virtual void update()
 	{
-		force.y *= -1;
+		double dt = Scene::DeltaTime();
+		vel += (force / mass) * dt;
+		pos += vel * dt;
+		obj.setPos(pos);
 	}
 
-	void revZ()
+	void stop()
 	{
-		force.z *= -1;
+		vel = Vec3{ 0, 0, 0 };
+		force = Vec3{ 0, 0, 0 };
 	}
 
-	void rev()
+	virtual void reflectX(Physics& wall, double e)
 	{
-		force *= -1;
+		Vec3 X = { 1,0,0 };
+
+		Quaternion q = wall.obj.orientation;
+		Vec3 inDir = vel;
+
+		// X,Y,Zに四元数で回転したものが各面の法線の候補
+		// 今回、制約として横面にのみ衝突することを考える
+		Vec3 Normal = (q * X).normalized();
+
+		Vec3 Res = inDir + (2 * Normal.dot(-inDir)) * Normal;
+
+		vel = Res * e;
 	}
 
-	Vec3 detect(Physics &obj)
+	virtual void reflectY(Physics& wall, double e)
 	{
-		double e = 0.5; //反発係数
-		double m1 = this->mass;
-		double m2 = obj.mass;
-		double M = (m1 * m2) / (m1 + m2);
-
-		Vec3 f1 = this->force;
-		Vec3 f2 = obj.force;
-
-		Vec3 v1 = f1 / m1;
-		Vec3 v2 = f2 / m2;
-
-		// 衝突面の法線ベクトル objからthisへの向き
-		Vec3 n = (this->pos - obj.pos).normalize();
-
-		// 撃力を計算する
-		Vec3 F = ( (1 + e) * M * (v1 - v2) * n ) + (M * (f1/m1 - f2/m2) * n ) * (n) * (1/Scene::DeltaTime());
-
-		// 撃力を物体に加える
-		this->setForce(this->force + F);
-		obj.setForce(obj.force - F);
+		Vec3 Y = { 0,1,0 };
 		
-		return F;
+		Quaternion q = wall.obj.orientation;
+		Vec3 inDir = vel;
+
+		// 制約として上面にのみ衝突することを考える
+		Vec3 Normal = (q * Y).normalized();
+
+		Vec3 Res = inDir + (2 * Normal.dot(-inDir)) * Normal;
+
+		vel = Res * e;
 	}
 
-
-};
-
-struct Phy_Test : public Physics
-{
-	Mesh mesh;
-
-	Phy_Test(Mesh _mesh, double _mass, Vec3 _pos, Vec3 _force)
-		: Physics(_mass, _pos, _force), mesh(_mesh)
+	virtual void reflectZ(Physics& wall, double e)
 	{
-	}
-	
+		Vec3 Z = { 0,0,1 };
 
-	void draw() const
-	{
-		mesh.draw(pos, Palette::Red);
+		Quaternion q = wall.obj.orientation;
+		Vec3 inDir = vel;
+
+		// X,Y,Zに四元数で回転したものが各面の法線の候補
+		// 今回、制約として上面にのみ衝突することを考える
+		Vec3 Normal = (q * Z).normalized();
+
+		Vec3 Res = inDir + (2 * Normal.dot(-inDir)) * Normal;
+
+		vel = Res * e;
 	}
 
-	
 };
 
 struct Prize : public Physics
 {
-	Mesh type;
-	Vec3 pos;
-	double th0; // 初期位相
+	Vec3 center;
+	double w_rad;
+	double radius;
+	double th0;
+	bool isHit;
 
-	Prize(const Mesh& _type, const Vec3& _pos, const Vec3& _force, const double& _th0)
-		: Physics(1.0, _pos, _force), type(_type), pos(_pos), th0(_th0)
+	/// @param mass 
+	/// @param vel 
+	/// @param force 
+	/// @param obj 
+	/// @param center 
+	/// @param w_rad 
+	Prize(double mass, Vec3 vel, Vec3 force, OrientedBox obj, Vec3 center, double w_rad, double th0)
+		: Physics(mass, vel, force, obj), center(center), w_rad(w_rad), isHit(false), th0(th0)
+	{
+		radius = (pos - center).length();
+	}
+
+	void draw() const override
+	{
+		obj.draw(Palette::Aquamarine);
+	}
+
+	void update() override
+	{
+		if (!isHit)
+		{
+			// Prizeはxz平面で円運動をする
+			double theta = w_rad * Scene::Time() + th0;
+			Vec3 dir = (pos - center).normalized(); // 中心への方向ベクトル
+
+			vel = dir * radius * w_rad;
+			force = dir * mass * radius * w_rad * w_rad;
+
+			pos = center + Vec3{ radius* Cos(theta), 0, radius* Sin(theta) };
+
+			obj.setPos(pos);
+		}
+		else
+		{
+			// Prizeは衝突した後、重力に従って落下する
+			vel += force / mass * Scene::DeltaTime();
+			pos += vel * Scene::DeltaTime();
+			obj.setPos(pos);
+		}
+
+		if (pos.length() > 10)
+		{
+			isHit = false;
+		}
+	}
+
+	void reflectY(Physics& wall, double e) override
+	{
+		Vec3 Y = { 0,-1,0 };
+
+		Quaternion q = wall.obj.orientation;
+		Vec3 inDir = vel;
+
+		Vec3 Normal = (q * Y).normalized();
+		Vec3 Res = inDir + (2 * Normal.dot(-inDir)) * Normal;
+
+		vel = Math::Abs(Res) * e;
+	}
+};
+
+struct Pusher : Physics
+{
+	double w_rad;
+
+	Pusher() : Physics(), w_rad(0.0) {}
+
+	/// @param mass 
+	/// @param vel 
+	/// @param force 
+	/// @param obj 
+	Pusher(double mass, Vec3 vel, Vec3 force, OrientedBox obj)
+		: Physics(mass, vel, force, obj)
+	{
+		w_rad = Math::ToRadians(90);
+		// T = 2pi / w (s)
+	}
+
+	void update() override
+	{
+		// xz平面で前後に動く
+		double dt = Scene::DeltaTime();
+		double theta = w_rad * Scene::Time();
+
+		pos += Vec3{ 0, 0, 1 } * Sin(theta) * dt;
+		obj.setPos(pos);
+	}
+
+	void draw() const override
+	{
+		obj.draw(Palette::Red);
+	}
+
+	void push(Physics &p)
+	{
+		// 衝突したPrizeを前方に押す
+		p.vel += Vec3{ 0, 0, 1 } * 10;
+		p.force += Vec3{ 0, 0, 1 } * 10;
+
+	}
+};
+
+struct Arm : Physics
+{
+	Arm() : Physics() {}
+
+	Arm(double mass, Vec3 vel, Vec3 force, OrientedBox obj)
+		: Physics(mass, vel, force, obj)
 	{
 	}
 
-	void draw() const
+	void update() override
 	{
-		type.draw(pos, Palette::Aquamarine);
+		if (KeySpace.pressed() && obj.orientation.getZ() < Quaternion::RotateZ(90_deg).getZ())
+		{
+			obj.orientation *= Quaternion::RotateZ(0.3);
+		}
+		else if (obj.orientation.getZ() > Quaternion::RotateZ(-70_deg).getZ())
+		{
+				obj.orientation *= Quaternion::RotateZ(-0.1);
+		}
 	}
 
-	void update(double w_rad)
+	void draw()
 	{
-		// yを固定軸としてxz平面で等速円運動
-		double radius = Math::Sqrt(pos.x * pos.x + pos.z * pos.z);
-		Vec3 nex = { radius * Math::Cos(w_rad * t + th0), pos.y, radius * Math::Sin(w_rad * t + th0)};
-		pos = nex;
+		obj.draw(Palette::Green);
 	}
 };
 
 struct Machine
 {
-	Vec3 center;
-	double w_rad;
-	double radius;
-	Array<Prize> goods;
+	Physics base;
+	Pusher pusher;
+	Arm arm;
 
-	void draw() const
+	Machine()
 	{
-		for (auto& g : goods)
-		{
-			g.draw();
-		}
+		base = Physics{ 1.0, {0,0,0}, {0,0,0}, OrientedBox{{0,0,2}, {4, 0.5, 4}} };
+		pusher = Pusher{ 1.0, {0,0,0}, {0,0,0}, OrientedBox{{0,0.5,0}, {4, 0.4, 2}} };
+		arm = Arm{ 1.0, {0,0,0}, {0,0,0}, OrientedBox{{-8,0,1}, {10, 0.1, 1.0}, Quaternion::RotateZ(-70_deg)} };
+	}
+
+	void draw()
+	{
+		base.draw();
+		pusher.draw();
+		arm.draw();
 	}
 
 	void update()
 	{
-		for (auto& g : goods)
-		{
-			g.update(w_rad);
-		}
-	}
-};
-
-struct Detection
-{
-	Vec3 pos;
-	double radius;
-	Sphere sphere;
-
-	void draw() const
-	{
-		sphere.draw(Palette::Black);
+		base.update();
+		pusher.update();
+		arm.update();
 	}
 
-	void update()
-	{
-		sphere = Sphere(pos, radius);
-	}
-
-	void detect(const Machine &machine)
-	{
-		for (auto& g : machine.goods)
-		{
-			
-		}
-	}
-
-};
-
-struct Capture
-{
-	Vec3 pos; // キャッチャーの位置
-	Vec3 center; // 回転の中心
-	double w_rad; // 回転速度(rad/s)
-	double radius; //回転の半径
-	Mesh hemisphere;
-	Detection detection;
-
-	Capture(const Vec3& _pos, const Vec3& _center, const double& _w_rad, const double& _radius, const Mesh& _hemisphere)
-		: pos(_pos), center(_center), w_rad(_w_rad), radius(_radius), hemisphere(_hemisphere)
-	{
-		detection.pos = pos;
-		detection.radius = hemisphere.boundingSphere().r - 0.1;
-	}
-
-	void draw(auto& uv) const
-	{
-		const ScopedRenderStates3D inActiveCull{ RasterizerState::SolidCullNone }; //カリングを両面無効に
-		hemisphere.draw(pos, Quaternion::RotateZ(w_rad * t + 180_deg), uv);
-		detection.draw();
-	}
-
-	void update()
-	{
-		// zを固定軸としてxy平面で回転運動のシミュレーション
-		// とりあえず等速円運動
-		Vec3 nex = { radius * Math::Cos(w_rad * t) + center.x, radius * Math::Sin(w_rad * t) + center.y, pos.z };
-		pos = nex;
-		detection.pos = pos;
-		detection.update();
-	}
 };
 
 void Main()
@@ -210,96 +268,78 @@ void Main()
 	const ColorF backgroundColor = ColorF{ 0.4, 0.6, 0.8 }.removeSRGBCurve();
 	const Texture uvChecker{ U"example/texture/uv.png", TextureDesc::MippedSRGB };
 	const MSRenderTexture renderTexture{ Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes };
-	DebugCamera3D camera{ renderTexture.size(), 120_deg, Vec3{ 0, 3, 10 } };
+	DebugCamera3D camera{ renderTexture.size(), 120_deg, Vec3{ 0, 3, 5 } };
 
-	Array<Prize> goods;
-	const Mesh torus{ MeshData::Torus(6.0, 2.0) };
-	for (int i = 0; i < 500; i++)
+	Array<Prize> prizes;
+	for (int i : step(100))
 	{
-		Mesh obj;
-		const Array<String> type = { U"Sphere", U"Box", U"Pyramid" };
-		String tp = type.choice();
-
-		// トーラスの一般式 中心までの距離D, 環の半径R, 環の中の角度u, 中心までの角度vとする
 		double D = 5.0;
-		double R = 1.0;
-		double u = Random(Math::Pi);
+		double R = 0.3;
+		double u = Random(Math::TwoPi);
 		double v = Random(Math::TwoPi);
 		double x = (D + R * Math::Cos(u)) * Math::Cos(v);
 		double z = (D + R * Math::Cos(u)) * Math::Sin(v);
 		double y = R * Math::Sin(u);
-		Vec3 pos = { x, y, z };
-
-		if (tp == U"Sphere")
-		{
-			// 中心pos, 半径rの球を生成
-			double r = 0.2;
-			obj = (Mesh)MeshData::Sphere(r);
-		}
-		else if (tp == U"Box")
-		{
-			// 辺の長さlの立方体を生成
-			double l = 0.3;
-			obj = (Mesh)MeshData::Box(l);
-		}
-		else if (tp == U"Pyramid")
-		{
-			// 底辺l, 高さhの四角錐を生成
-			double l = 0.3;
-			double h = 0.3;
-			obj = (Mesh)MeshData::Pyramid(l, h);
-		}
-		else
-		{
-			Print << U"Not Found Error in goods select";
-		}
-
-		goods.push_back(Prize(obj, pos, Vec3{ 0,0,0 }, v));
+		Prize p{ 1.0, Vec3{ 0, 0, 0 }, Vec3{ 0, 0, 0 }, OrientedBox{ Vec3{x,y,z}, Vec3{0.3, 0.3, 0.3}},  Vec3{0,1,0}, Math::ToRadians(90), v};
+		prizes.push_back(p);
 	}
-	// machine{ center, w_rad, radius, goods }
-	Machine machine{ Vec3{ 0, 0, 0 }, (1080_deg * Math::Pi / 180.0), 20.0, goods };
 
-	Mesh hemisphere{ MeshData::Hemisphere(1.0) };
-	// capture{ pos, center, w_rad, radius, hemisphere }
-	Capture capture{ Vec3{ 0, 0, 0 }, Vec3{ 0, 1.5, 5 }, 10*(360_deg * Math::Pi / 180.0), 1.0 , hemisphere };
+	Machine machine;
+	Box capture = Box::FromPoints({ -2, -2, 4 }, { 2, 0, 6});
 
-	Phy_Test test1{ (Mesh)MeshData::Box(1.0), 1.0, Vec3{ -20, -10, 0 }, Vec3{ 50, 0, 0 } };
-	Phy_Test test2{ (Mesh)MeshData::Box(1.0), 1.0, Vec3{ 20, -10, 0 }, Vec3{ -50, 0, 0 } };
-
-
-
+	int point = 0;
 	while (System::Update())
 	{
-		t = Scene::Time();
 		camera.update(2.0);
 		Graphics3D::SetCameraTransform(camera);
 
 		// 3D 描画
 		{
 			const ScopedRenderTarget3D target{ renderTexture.clear(backgroundColor) };
-			Plane{ Vec3{0,-30, 0}, 64 }.draw(uvChecker);
-			//torus.draw({ 0, 0, 0 }, Quaternion::RotateY(t * 30_deg), uvChecker);
 			{
 				const ScopedRenderStates3D blend{ BlendState::OpaqueAlphaToCoverage };
+
 				machine.update();
 				machine.draw();
 
-				capture.update();
-				capture.draw(uvChecker);
-				capture.detection.detect(machine);
-			}
+				capture.draw(Palette::Black);
+				for (auto& p : prizes)
+				{
+					p.update();
+					p.draw();
 
-			{
-				test1.update();
-				test1.draw();
-				test2.update();
-				test2.draw();
+					if (p.obj.intersects(machine.arm.obj))
+					{
+						p.isHit = true;
+						p.force = Vec3{ 0, -9.8, 0 };
+						p.reflectY(machine.arm, 0.9);
+					}
 
-				if (Key1.down()) {
-					Print << test1.detect(test2);
-					
+					if (p.obj.intersects(machine.pusher.obj))
+					{
+						machine.pusher.push(p);
+					}
+
+					if (p.obj.intersects(machine.base.obj))
+					{
+						p.force = Vec3{ 0, 0, 0 };
+						p.reflectY(machine.base, 0.1);
+					}
+
+					if (p.obj.intersects(capture))
+					{
+						point++;
+					}
+
+					for (auto& q : prizes)
+					{
+						if (p.obj.intersects(q.obj) && Abs(p.vel.y) < 0.1 && Abs(q.vel.y) < 0.1)
+						{
+							p.reflectZ(q, 0.9);
+						}
+					}
+
 				}
-
 			}
 		}
 
@@ -308,6 +348,11 @@ void Main()
 			Graphics3D::Flush();
 			renderTexture.resolve();
 			Shader::LinearToScreen(renderTexture);
+		}
+
+		// 2D 描画
+		{
+			FontAsset(U"Score")(U"Score: ", point).draw(10, 10, Palette::White);
 		}
 
 	}
